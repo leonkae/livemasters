0cc2a05a684014ef8169b8322649c7bc69369d05
leonkae <leon29kae@gmail.com>
2025-02-07 14:41:19 +0300

    modify:multiple

--- liveapp/models.py
+++ liveapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=450)
+    content = models.TextField()
+    image = models.ImageField(upload_to="images/")
+    
+    def __str__(self):
+        return self.title
--- liveapp/views.py
+++ liveapp/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from .models import *
 # Create your views here.
 
 class HomeView(ListView):
     template_name = "liveappTemp/index.html"
     
     def get(self, request):
+        blogs = BlogPost.objects.all()
         
         context = {
-            
+            'blogs':blogs
         }
         
         return render(request, self.template_name,context)
