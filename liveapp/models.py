7e3a580f81647f1c95e3af6373a86dc07e9c157a
leonkae <leon29kae@gmail.com>
2025-03-22 13:08:45 +0300

    modify:models, global, contact

--- liveapp/models.py
+++ liveapp/models.py
@@ -13,3 +13,13 @@ class BlogPost(models.Model):
     
     def __str__(self):
         return self.title
+    
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.subject
